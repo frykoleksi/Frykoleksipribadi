0c2c7527e512cb2e4850179d97b23e9664d2ffe6
frykoleksi <animefavorit82@gmail.com>
2026-06-11 19:56:59 +0700

    Add files via upload

---
+++ database.js
@@ -0,0 +1,105 @@
+// database.js
+const databaseVideoManual = {
+    "52305": { 
+        "title": "Otonari no Tenshi sama", // Tambahkan baris ini disetiap data anime baru bro!
+        "sinopsis": "Amane Fujimiya adalah cowok SMA berantakan yang tinggal bertetangga dengan Mahiru Shiina, gadis tercantik di sekolah yang dijuluki "Malaikat". Hubungan mereka dimulai saat Amane meminjamkan payungnya di hari hujan, yang membuat Amane jatuh sakit keesokan harinya. Merasa berutang budi dan tidak tega melihat gaya hidup Amane, Mahiru mulai datang setiap hari untuk memasak dan merawatnya. Demi menghindari rumor, mereka merahasiakan hubungan ini, namun interaksi harian tersebut perlahan menumbuhkan cinta yang tulus dan manis di antara keduanya.",
+        "genre": ["Romance", "School", "Slice of Life"],
+        "episodes": {
+            "1": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=7f7b55&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-01-1080p.mp4",
+                "server2": "https://mega.nz/embed/Yup2RTyZ#V_eERoc6TvM4CzEz5CzkEF5b5u0TmQQf3XGJuaDrcCA"
+            },
+            "2": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=5091ba&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-02-1080p.mp4",
+                "server2": "https://mega.nz/embed/tjQHnTLB#Gu0VDkwD_YVmsbjW3wtHy8t2YUEYvHsDYhleLjqCrp4"
+            },
+            "3": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=88f4ef&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-03-1080p.mp4",
+                "server2": "https://mega.nz/embed/ZjB11A5A#LOrmUbDyICvp8g89ye-LaPt0keSsc-caJVHHIPvJ1Bk"
+            },
+            "4": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=89620f&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-04-1080p.mp4",
+                "server2": "https://mega.nz/embed/sjYRlAAR#NNnHshr24NRFVUIevBxQ-MZ59unbAuLhorhtqoab85Q"
+            },
+            "5": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=e52c24&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-05-1080p.mp4",
+                "server2": "https://mega.nz/embed/lvZUyLrK#Ug2DfIAnN-ZqmtJELdUQT6F8qMUhbE2zMzIfTChocQI"
+            },
+            "6": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=a026ec&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-06-1080p.mp4",
+                "server2": "https://mega.nz/embed/1yABUDRD#nrHHPwgXBNCOePVHUqruHNau1p4tDO2WL03ybKxkaiQ"
+            },
+            "7": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=6b15b6&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-07-1080p.mp4",
+                "server2": "https://mega.nz/embed/8nA2mRJJ#XFteHdgXTUh4u-FfMDft1iK-261_-1SqyhmKHxHxByA"
+            },
+            "8": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=e57af7&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-08-1080p.mp4",
+                "server2": "https://mega.nz/embed/12oE2RIA#5UdDresqeugicJLT8E75YjnUfbCklDjoKuUCPU0_EUQ"
+            },
+            "9": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=6bce1b&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-09-1080p.mp4",
+                "server2": "https://mega.nz/embed/AyA3gKYD#o9YCWVpY-rHdNiKHwlSP5sLvj7fVwKtNE3zQlB7s7io"
+            },
+            "10": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=0ec375&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-10-1080p.mp4",
+                "server2": "https://mega.nz/embed/xzggjIKD#gVhYguyw5X0IpH6Zk0L0GFPijPBzxvG4EYh-qq34N4I"
+            },
+            "11": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=bc1215&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-11-1080p.mp4",
+                "server2": "https://mega.nz/embed/cvgjHD7Q#1PYDlrtXKxvt8AE8iOJP2d1MYA0OwJX_lxDAhLja7HI"
+            },
+            "12": {
+                "server1": "https://dlgan.halahgan.com/streaming.php?id=3b51de&name=Nimegami-Otonari-no-Tenshi-sama-BD-Ep-12-1080p.mp4",
+                "server2": "https://mega.nz/embed/t6pQmaZY#W569YJo7UepDdwNlYe3o4KlbFE9v3uq8p00SiQ3GJes"
+            }
+            // Kamu tinggal teruskan ke bawah untuk episode berikutnya...
+        }
+    },
+    "12345": { // Contoh ID Anime Lainnya
+        "sinopsis": "Setelah resmi berpacaran di akhir musim pertama, Amane Fujimiya dan Mahiru Shiina kini menjalani babak baru hubungan mereka yang jauh lebih romantis. Tidak lagi sekadar bersembunyi sebagai tetangga, mereka kini harus belajar menghadapi reaksi teman-teman di sekolah setelah status hubungan mereka mulai terbuka. Musim kedua ini akan berfokus pada perkembangan cinta mereka yang semakin intim, momen saling terbuka tentang masa lalu, serta keseharian manis yang dijamin bikin baper.",
+        "genre": ["Romance", "School", "Slice of Life"],
+        "episodes": {
+            "1": {
+                "server1": "https://filedon.co/embed/lJ0gf9HtM1",
+                "server2": "https://acefile.co/player/111479033"
+            },
+            "2": {
+                "server1": "https://filedon.co/embed/j8K3s2Q5F6",
+                "server2": "https://mega.nz/embed/J6kjBIYT#k4QYnUnfWaGqnls4U8M2EPMCGX2Aaml8P4S-4SspbTU"
+            },
+            "3": {
+                "server1": "https://filedon.co/embed/3O4pDFVNRo",
+                "server2": "https://mega.nz/embed/J6kjBIYT#k4QYnUnfWaGqnls4U8M2EPMCGX2Aaml8P4S-4SspbTU"
+            },
+            "4": {
+                "server1": "https://filedon.co/embed/9vGEYSvF5J",
+                "server2": "https://mega.nz/embed/diwwRLLZ#Dr6T1mr-svj6H6Nt3p58pN8rDR50UgVZFht2GAZaSjQ"
+            },
+            "5": {
+                "server1": "https://filedon.co/embed/PjVVqgCdDL",
+                "server2": "https://mega.nz/embed/4A1HyQTI#IeTH7NpQ0STo7CfScMfDNxAIHULJfU2FI25J-l3FK2M"
+            },
+            "6": {
+                "server1": "https://filedon.co/embed/9vCmNWVObP",
+                "server2": "https://mega.nz/embed/d2khAAyD#lqlIElLYRRYkinAqxDrytH_v1tp3ANfebbAKPi5xDPw"
+            },
+            "7": {
+                "server1": "https://filedon.co/embed/vO6FXZ4DTg",
+                "server2": "https://mega.nz/embed/24om3QDQ#lgBwEAUt7UbbPs-RJP4fiSrRFqpeGjk_lwNy0Cw-r0c"
+            },
+            "8": {
+                "server1": "https://filedon.co/embed/yiuxe35fPP",
+                "server2": "https://mega.nz/embed/smdzALiY#uDjQuLT-KzSZJLoL9ssy3Fwf1kaAatdVal2p1ef3sXs"
+            },
+            "9": {
+                "server1": "https://filedon.co/embed/SncokziPpk",
+                "server2": "https://mega.nz/embed/l6V0mL4b#bYZWtHNYyB6jVRhDbqwUNou_z9gbeUDUll_78bqM_mA"
+            },
+            "10": {
+                "server1": "https://filedon.co/embed/K6QacGjsOb",
+                "server2": "https://mega.nz/embed/qm4WXKaS#kXJrqDpXtZF-VqePz_vGIliNnE06xI-pGVB09YizmcE"
+            }
+        }
+    }
+};
